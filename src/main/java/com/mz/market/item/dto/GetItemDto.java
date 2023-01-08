801311bdde8b458d22ee346f46bb31d7624076d7
jeongjuryu <totw2018@naver.com>
2023-01-08 20:22:38 +0900

    feat: Item Dto 추가

---
+++ src/main/java/com/mz/market/item/dto/GetItemDto.java
@@ -0,0 +1,16 @@
+package com.mz.market.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+
+public class GetItemDto {
+    @Getter
+    public static class Request { }
+    @Getter
+    @Builder
+    public static class Response {
+
+    }
+}
---
+++ src/main/java/com/mz/market/item/dto/PostItemDto.java
@@ -0,0 +1,14 @@
+package com.mz.market.item.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class PostItemDto {
+    @Getter
+    public static class Request { }
+    @Getter
+    @Builder
+    public static class Response {
+
+    }
+}
